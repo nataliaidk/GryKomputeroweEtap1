9214057e8f31e6c27a61e21ccb657348781dab91
marg4ryn <poweski.jan@gmail.com>
2026-04-09 17:44:56 +0200

    Button click sound

--- scripts/game/death_screen.gd
+++ scripts/game/death_screen.gd
@@ -1,9 +1,19 @@
 extends Control
 
+var hover_sound := preload("res://assets/sounds/button hover.mp3")
+
+@onready var audio = $AudioStreamPlayerButton
+@onready var back_button = $VBoxContainer/Back
 @onready var timer_label = $TimerLabel
 
 func _ready():
 	timer_label.text = "Survived for %02d' %02ds" % [int(GameTimer.seconds()) / 60, int(GameTimer.seconds()) % 60]
+	back_button.mouse_entered.connect(_on_hover)
+
+func _on_hover():
+	audio.stream = hover_sound
+	audio.play()
+	
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/main_menu.tscn")
--- scripts/game/main_menu.gd
+++ scripts/game/main_menu.gd
@@ -1,9 +1,21 @@
 extends Control
 
+var hover_sound := preload("res://assets/sounds/button hover.mp3")
+
+@onready var audio = $AudioStreamPlayerButton
+@onready var start_button = $VBoxContainer/Start
+@onready var exit_button = $VBoxContainer/Exit
+
+func _ready():
+	start_button.mouse_entered.connect(_on_hover)
+	exit_button.mouse_entered.connect(_on_hover)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_hover():
+	audio.stream = hover_sound
+	audio.play()
